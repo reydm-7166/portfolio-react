635eb6e4178fd6a76c9e89c905bd82b4f7068e75
Reymond <reymond.domingo.716@gmail.com>
2023-07-10 16:19:03 +0800

    adjusted pc screen size

--- src/Components/Sections/About.js
+++ src/Components/Sections/About.js
@@ -29,24 +29,23 @@ function About()
             </div>
 
             <div id="DetailsContainer" className="mt-2 flex flex-wrap md:w-full md:h-full">
-                <div id="Image" className="bg-white rounded-[22px] w-80 m-auto h-64 sm:w-80 md:w-[33%] md:h-[300px]">
+                <div id="Image" className="bg-white rounded-[22px] w-80 m-auto h-64 sm:w-[300px] md:w-[300px] md:h-[310px]">
                     <img src={Tiger} alt="" className="w-full h-full rounded-[22px]"/>
                 </div>
-                <div id="detailsSection" className="w-full min-h-[300px] h-fit mt-12 md:w-4/6 md:mt-0">
+                <div id="detailsSection" className="w-full min-h-[300px] h-fit mt-12 md:w-[58%] lg:w-[66%] md:mt-0">
                     <div id="cardContainer" className="h-1/2 px-2 flex flex-wrap justify-evenly">
-                        
                         <SkillCards data={obj.experience}></SkillCards>
                         <SkillCards data={obj.projects}></SkillCards>
                         <SkillCards data={obj.degree}></SkillCards>
                     </div>
-                    <div id="Objectives" className="h-fit px-2 py-1 w-full flex mt-8 md:mt-3 justify-center lg:mt-10">
+                    <div id="Objectives" className="h-[112px] px-2 py-1 w-full flex mt-5 md:mt-5 justify-center">
                         <div className="m-2 min-h-fit">
                             <p className="ms-4 text-center md:text-start text-[#A9ADB4]">
                             I'm a backend developer who is passionate about implementing clean code. I strive to contribute to a company where I can learn and grow while making a positive impact.
                             </p>
                         </div>
                     </div>
-                    <div id="ConnectButton" className="ms-4 mt-4 h-[50px] text-center md:text-start md:mt-5 lg:mt-7">
+                    <div id="ConnectButton" className="ps-4 mt-4 h-[50px] text-center md:text-start md:mt-5">
                         <button className="w-42 bg-[#A395E9] md:ms-3 hover:bg-blue-700 text-black poppins-font py-2 px-4 rounded md:mt-3 text-xs sm:text-sm lg:text-sm">Connect with me</button>
                     </div>
                 </div>
